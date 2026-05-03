ce31e75595c4d78ce74c4c3bf56ed9567bd384a4
Zarah Baloch <115739350+zarahbaloch@users.noreply.github.com>
2026-05-03 16:18:06 -0400

    Add: python-string-looping-shorthand - submission-0

---
+++ Python For Beginners/python-string-looping-shorthand/submission-0.py
@@ -0,0 +1,11 @@
+def print_string_characters(word1: str, word2: str) -> None:
+    pass
+
+    for char in word1:
+        print(char)
+    for char in word2: 
+        print(char)
+
+
+# do not modify below this line
+print_string_characters("Hello, World!", "Good Job!")
