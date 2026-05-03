d000254a6fbed1e6fb84e9fadaea4b44fa48760d
Zarah Baloch <115739350+zarahbaloch@users.noreply.github.com>
2026-05-02 20:56:31 -0400

    Add: python-dynamic-typing - submission-2

---
+++ Python For Beginners/python-dynamic-typing/submission-2.py
@@ -0,0 +1,14 @@
+variable= 4
+print(type(variable))
+
+variable= 74.2
+print(type(variable))
+
+variable= True 
+print(type(variable))
+
+variable= "heyy"
+print(type(variable))
+
+variable= [9,0,5]
+print(type(variable))
